3dcd2cb1b125e343ca831c13883f9edb5b31406f
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-03-06 21:28:46 +0900

    feat: add speechRecognition feature

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react'
+
 function App(): JSX.Element {
+  const [words, setWords] = useState<string[]>([])
+
+  const WindowSpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+  const recognition = new WindowSpeechRecognition()
+  recognition.onresult = (e) => {
+    const result = e.results[0][0].transcript
+    setWords((prev) => [...prev, result])
+  }
+
+  const handleClick = () => {
+    recognition.start()
+  }
+
   return (
     <>
-      <input placeholder="안녕하세요" />
+      <div onClick={handleClick}>음성인식 시작</div>
+      <div>{words.toString()}</div>
     </>
   )
 }
-
 export default App
