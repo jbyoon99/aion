04e3576a2d90317503d38a6d8f10142171136f3e
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-03-09 22:22:27 +0900

    chore: update renderer path resolve rule

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@static': resolve('src/static')
       }
     },
     plugins: [tailwindcss()]
