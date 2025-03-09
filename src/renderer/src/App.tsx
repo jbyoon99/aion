27b0d2693ad08cff61597b1c3385c79d8ca7d781
JaeBin Yoon <dbswoqls89@snu.ac.kr>
2025-03-09 22:23:43 +0900

    feat: set react-router-dom

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
+import { Routes, HashRouter, Route } from 'react-router-dom'
+import { Pending } from './views/Pending'
+import { Settings } from './views/Settings'
 
 function App(): JSX.Element {
-  const [words, setWords] = useState<string[]>([])
-
-  const WindowSpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-  const recognition = new WindowSpeechRecognition()
-  recognition.onresult = (e) => {
-    const result = e.results[0][0].transcript
-    setWords((prev) => [...prev, result])
-  }
-
-  const handleClick = () => {
-    recognition.start()
-  }
-
   return (
-    <>
-      <div onClick={handleClick}>음성인식 시작</div>
-      <div>{words.toString()}</div>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Pending />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </HashRouter>
   )
 }
 export default App
